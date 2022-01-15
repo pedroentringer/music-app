6940c7ba88ec6d2e948a0030ef76578f432aca59
Pedro Entringer <pedroentringer97@gmail.com>
2022-01-15 16:03:30 +0100

    fix: progressBarAnimation and width

--- src/components/progressBar/index.tsx
+++ src/components/progressBar/index.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { useDynamicAnimation } from 'moti';
 import { runOnJS, useAnimatedGestureHandler } from 'react-native-reanimated';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { GestureEventPayload, PanGestureHandler, PanGestureHandlerEventPayload } from 'react-native-gesture-handler';
 
 import { 
   Container,
@@ -12,20 +12,42 @@ import {
 
 import { PlayerContext } from '../../providers/player';
 import { AVPlaybackStatus } from 'expo-av';
+import { LayoutChangeEvent } from 'react-native';
+import { TextBold } from '../texts';
 
 const ProgressBar = () => {
 
   const playerContext = useContext(PlayerContext)
 
+  const [isDrag, setIsDrag] = useState(false)
+  const [maxWidth, setMaxWidth] = useState(0)
+
   const animation = useDynamicAnimation(() => ({
     width: 0
   }))
 
-  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
-    const current = animation.current?.width as number || 0 
+  const onLayoutBar = (layoutEvent: LayoutChangeEvent) => {
+    const { width } = layoutEvent.nativeEvent.layout;
+
+    if(width && width > 0){
+      setMaxWidth(width)
+    }
+
+  }
+
+  const onPlaybackStatusUpdate = ({ playableDurationMillis, positionMillis }: AVPlaybackStatus) => {
+    setIsDrag((prevIsDrag) => {
+      if(!prevIsDrag){
+        const progressInPercent =  (100 * positionMillis) / playableDurationMillis;
     
-    animation.animateTo({
-      width: current + 1
+        const newWidth =  ( progressInPercent * maxWidth) / 100;
+    
+        animation.animateTo({
+          width: newWidth
+        })
+      }
+
+      return prevIsDrag
     })
   }
 
@@ -37,34 +59,46 @@ const ProgressBar = () => {
   
   }, [playerContext.player.playingNow.sound])
 
+  const processGestureNewWidth = async (event: Readonly<GestureEventPayload & PanGestureHandlerEventPayload>) => { 
+    if(playerContext.player.playingNow.sound){
+      const { playableDurationMillis } = await playerContext.player.playingNow.sound.getStatusAsync()
+
+      const progressInPercent =  (100 * event.absoluteX) / maxWidth;
+  
+      const newPositionInMillis =  ( progressInPercent * playableDurationMillis) / 100;
+
+      await playerContext.setPositionInMillis(newPositionInMillis)
+      
+    }
+    setIsDrag(false)
+  }
+
   const gestureHandler = useAnimatedGestureHandler({
-    onStart: async () => {
-      runOnJS(playerContext.handlePause)()
+    onStart: () => {
+      runOnJS(setIsDrag)(true)
     },
     onActive: (event) => {
       animation.animateTo({
         width: event.absoluteX
       })
     },
-    onEnd: async (event) => {
-      console.log(event)
-      
-      //converter o X em Millis para setar a posição
-
-      runOnJS(playerContext.setPositionInMillis)(30000)
-      runOnJS(playerContext.handlePlay)()
+    onEnd: (event) => {
+      runOnJS(processGestureNewWidth)(event)
     },
   });
 
   return (
+    <>
+          <TextBold>isDrag: {JSON.stringify(isDrag)}</TextBold>
     <PanGestureHandler onGestureEvent={gestureHandler}>
       <Container>
         <BarContainer>
-          <Bar />
+          <Bar onLayout={onLayoutBar}/>
           <Progress state={animation} transition={{type: 'timing', duration: 0}}/>
         </BarContainer>
       </Container>
     </PanGestureHandler>
+    </>
   )
 };
 
