dd41e000b09b51b030d095afc1b7ca7d9e98b348
Pedro Entringer <pedroentringer97@gmail.com>
2022-01-17 16:59:33 +0100

    remove modules

--- App.tsx
+++ App.tsx
@@ -3,27 +3,16 @@ import AppIndex from "./src/AppIndex";
 import ThemeProvider from './src/providers/theme';
 import PlayerProvider from './src/providers/player';
 
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
-
 import 'react-native-gesture-handler';
 
-export default function App() {
-    let [fontsLoaded] = useFonts({
-        'Belgacon-Regular': require('./src/fonts/belgacom-regular.ttf'),
-        'Belgacon-Bold': require('./src/fonts/belgacom-bold.ttf'),
-        'Belgacon-UltraLight': require('./src/fonts/belgacom-ultralight.ttf'),
-    });
-    
-      if (!fontsLoaded) {
-        return <AppLoading />;
-      } else {
-        return (
-            <ThemeProvider>
-                <PlayerProvider>
-                  <AppIndex />
-                </PlayerProvider>
-            </ThemeProvider>
-        );
-      }
+export default function App() {    
+      
+  return (
+      <ThemeProvider>
+          <PlayerProvider>
+            <AppIndex />
+          </PlayerProvider>
+      </ThemeProvider>
+  );
+
 }
